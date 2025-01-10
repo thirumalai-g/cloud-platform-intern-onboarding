b9fe26a0838a3e20d4bbb3ea3cb53093a59c5a85
pmi-thiruvenkadakrishnan-s <thiruvenkadakrishnan@poshmark.com>
2025-01-10 16:04:51 +0530

    RubyBasics

--- condition.rb
+++ condition.rb
@@ -66,24 +66,38 @@
 #     puts "increasing #{i}"
 #     raise if i>2
 #   rescue
-#       retry
+#     break
 #   end
 # end
 
-def geeks
-  attempt_again = true
-  p 'checking'
-  begin
-    # This is the point where the control flow jumps
-    p 'crash'
-    raise 'foo'
-  rescue Exception => e
-    if attempt_again
-      attempt_again = false
-      # Using retry
-      retry
-    end
-  end
-end
+# def geeks
+#   attempt_again = true
+#   p 'checking'
+#   begin
+#     # This is the point where the control flow jumps
+#     p 'crash'
+#     raise 'foo'
+#   rescue Exception => e
+#     if attempt_again
+#       attempt_again = false
+#       # Using retry
+#       retry
+#     end
+#   end
+# end
+#
+# geeks()
 
-geeks()
+
+
+#begin end
+puts "it will"
+
+BEGIN{
+  a = 4
+  b = 3
+  c = a+b
+  puts "the value of add a + b is #{c}"
+}
+
+
