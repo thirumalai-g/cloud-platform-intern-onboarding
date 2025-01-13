3af63130ccdcfe11a13bd23004281d75fc81abdb
pmi-thiruvenkadakrishnan-s <thiruvenkadakrishnan@poshmark.com>
2025-01-13 17:22:06 +0530

    ExceptionHandling

---
+++ Ruby/ExceptionHandling.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+begin
+  result = 10/0
+rescue ZeroDivisionError => e
+
+  puts "Error: #{e.class}: #{e.message}"
+end
+
+
+
+begin
+  result =10/2
+
+rescue ZeroDivisionError => e
+  puts "Error: #{e.class}: #{e.message}"
+else
+  puts "No Error: #{result}"
+ensure
+  puts "Excution completed"
+end
+
+
+#  own exception
+
+def check_age(age)
+  raise "Age must be a positive number" if age<=0
+  puts"Valid age:#{age}"
+end
+
+begin
+  check_age(-1)
+rescue => e
+  puts "caught exception: #{e.message}"
+ensure
+  puts "age verification"
+end
