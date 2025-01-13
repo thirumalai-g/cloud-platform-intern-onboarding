8c3912d71a64375678179cbfe753c67561a58d8c
pmi-thiruvenkadakrishnan-s <thiruvenkadakrishnan@poshmark.com>
2025-01-13 14:58:02 +0530

    Encapsulation.rb

---
+++ Encapsulation.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# Encapsulation of wrapping up of data under asingle unit
+
+
+class Encapsulation
+  def initialize(id,name,addr)
+    @id = id
+    @name = name
+    @addr = addr
+  end
+
+  def display
+    puts "Customer id: #@id"
+    puts "Name: #@name"
+    puts "Address: #@addr"
+  end
+end
+
+
+cust1 = Encapsulation.new("1","krish","chennai")
+
+cust2 = Encapsulation.new("2","thiru","usa")
+
+cust1.display
+
+cust2.display
